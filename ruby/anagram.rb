cacb25059cb449b67d6b7492dd0854548ae18096
Dylan Andrews <dylanandr@gmail.com>
2015-11-03 12:07:47 -0600

    make bob more modular

--- ruby/anagram.rb
+++ ruby/anagram.rb
@@ -1,5 +1,6 @@
 class Anagram
 
+  attr_reader :word
   def initialize(word)
     @word = word
   end
@@ -8,7 +9,7 @@ class Anagram
     anagram_list = []
     sorted_list = sort_list(word_list)
     sorted_list.each_with_index do |sorted_word, index|
-      if sorted_word == canonical(@word) && word_list[index].downcase != @word.downcase
+      if sorted_word == canonical(word) && word_list[index].downcase != word.downcase #valid word method
         anagram_list << word_list[index]
       end
     end
@@ -19,8 +20,44 @@ class Anagram
     word.downcase.split('').sort
   end
 
-  def sort_list(word_array)
-    word_array.map {|word| canonical(word)}
+  def sort_list(potential_anagrams)
+    potential_anagrams.map {|word| canonical(word)}
   end
 
 end
+
+class Anagram
+
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match(word_list)
+    anagram_list = []
+    sorted_list = sort_list(word_list)
+    sorted_list.each_with_index do |sorted_word, index|
+      if sorted_word == canonical(word) && word_list[index].downcase != word.downcase #valid word method
+        anagram_list << word_list[index]
+      end
+    end
+    anagram_list
+  end
+
+  def canonical(word)
+    word.downcase.split('').sort
+  end
+
+  def sort_list(potential_anagrams)
+    potential_anagrams.map {|word| canonical(word)}
+  end
+
+end
+
+
+
+
+# should I do @ word or attr_reader
+# check on namings
+# is canonical word stringing of methods ok?
+# should the anagram list be created in initialize?
--- ruby/bob.rb
+++ ruby/bob.rb
@@ -1,39 +1,65 @@
 class Bob
 
   def hey(remark)
-    if remark == "WATCH OUT!"
-      "Whoa, chill out!"
-    elsif remark.strip.empty?
+    if blank?(remark)
       "Fine. Be that way!"
-    elsif (remark =~ /[a-zA-Z]/) == nil && (remark.end_with? "?") == false
+    elsif other?(remark)
       "Whatever."
-    elsif (remark.end_with? "?") && ((remark =~ /[a-z]/) || (remark =~ /\d/))
+    elsif pure_question?(remark)
       "Sure."
-    elsif remark.upcase == remark
+    elsif yelling?(remark)
       "Whoa, chill out!"
     else
       "Whatever."
     end
   end
 
-end
+  def blank?(remark)
+    remark.strip.empty?
+  end
 
-class Bob
+  def yelling?(remark)
+    remark.upcase == remark
+  end
 
-  def hey(remark)
-    if remark == "WATCH OUT!"
-      "Whoa, chill out!"
-    elsif remark.strip.empty?
-      "Fine. Be that way!"
-    elsif (remark =~ /[a-zA-Z]/) == nil && (remark.end_with? "?") == false
-      "Whatever."
-    elsif (remark.end_with? "?") && ((remark =~ /[a-z]/) || (remark =~ /\d/))
-      "Sure."
-    elsif remark.upcase == remark
-      "Whoa, chill out!"
-    else
-      "Whatever."
-    end
+  def pure_question?(remark)
+    (remark.end_with? "?") && not_all_caps?(remark)
+  end
+
+  def other?(remark)
+    no_letters?(remark) && not_question?(remark)
+  end
+
+  def no_letters?(remark)
+    (remark =~ /[a-zA-Z]/) == nil
+  end
+
+  def not_question?(remark)
+    (remark.end_with? "?") == false
+  end
+
+  def not_all_caps?(remark)
+    ((remark =~ /[a-z]/) || (remark =~ /\d/))
   end
 
 end
+
+# class Bob
+
+#   def hey(remark)
+#     if remark == "WATCH OUT!"
+#       "Whoa, chill out!"
+#     elsif remark.strip.empty?
+#       "Fine. Be that way!"
+#     elsif (remark =~ /[a-zA-Z]/) == nil && (remark.end_with? "?") == false
+#       "Whatever."
+#     elsif (remark.end_with? "?") && ((remark =~ /[a-z]/) || (remark =~ /\d/))
+#       "Sure."
+#     elsif remark.upcase == remark
+#       "Whoa, chill out!"
+#     else
+#       "Whatever."
+#     end
+#   end
+
+# end
