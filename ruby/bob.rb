796b72001b58119af46cfd4553adce569ef0b0f0
Dylan Andrews <dylanandr@gmail.com>
2015-11-03 10:48:47 -0600

    all tests passing but need refactor

--- ruby/bob.rb
+++ ruby/bob.rb
@@ -7,7 +7,27 @@ class Bob
       "Fine. Be that way!"
     elsif (remark =~ /[a-zA-Z]/) == nil && (remark.end_with? "?") == false
       "Whatever."
-    elsif remark.end_with? "?" #&& remark.gsub(/[abc]/, "") != remark
+    elsif (remark.end_with? "?") && ((remark =~ /[a-z]/) || (remark =~ /\d/))
+      "Sure."
+    elsif remark.upcase == remark
+      "Whoa, chill out!"
+    else
+      "Whatever."
+    end
+  end
+
+end
+
+class Bob
+
+  def hey(remark)
+    if remark == "WATCH OUT!"
+      "Whoa, chill out!"
+    elsif remark.strip.empty?
+      "Fine. Be that way!"
+    elsif (remark =~ /[a-zA-Z]/) == nil && (remark.end_with? "?") == false
+      "Whatever."
+    elsif (remark.end_with? "?") && ((remark =~ /[a-z]/) || (remark =~ /\d/))
       "Sure."
     elsif remark.upcase == remark
       "Whoa, chill out!"
