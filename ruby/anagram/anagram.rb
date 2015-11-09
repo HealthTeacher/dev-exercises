436583bdc4251b2ba730208838601b64f723640a
Dylan Andrews <dylanandr@gmail.com>
2015-11-09 10:23:45 -0600

    refactoring both solutions

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -7,12 +7,13 @@ class Anagram
   def match(potential_angrms)
     anagram_list = []
     remove_identical_words!(potential_angrms)
-    potential_angrms.each_with_index do |word, index|
-      if canonical(word) == canonical(@word)
-        anagram_list << potential_angrms[index]
-      end
-    end
-    anagram_list
+    # potential_angrms.each_with_index do |word, index|
+    #   if canonical(word) == canonical(@word)
+    #     anagram_list << potential_angrms[index]
+    #   end
+    # end
+    # anagram_list
+    potential_angrms.select {|word| canonical(word) == canonical(@word)}
   end
 
   def canonical(word)
