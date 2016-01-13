446299ff6299c2e21423146320882ea7c5c7d913
Jerry Sims <gerald@artistgrowth.com>
2016-01-13 13:21:12 -0600

    Reformat anagram.rb for DRYness, readability, ruby-ness.
    - Changed the match method so that a single array is built using the select method instead of the more for...in method that I used before. This also removed the unnecessary step in which I had assigned an empty array to a variable.
    - Broke out criteria of a match to their own one-line methods for readability.
    - Removed unused attr_reader.

--- ruby/anagram.rb
+++ ruby/anagram.rb
@@ -1,16 +1,22 @@
 class Anagram
-  attr_reader :root_word
 
   def initialize(root_word)
     @root_word = root_word
   end
 
   def match(candidates)
-    anagrams = []
-    for c in candidates do
-      anagrams.push(c) if (c.downcase != self.root_word.downcase) && (c.downcase.split(//).sort == self.root_word.downcase.split(//).sort)
-    end
-    anagrams
+    candidates.select{|c| self.is_anagram?(c) }
   end
 
+  def is_anagram?(word)
+    self.not_equal(word) && self.has_same_letters(word)
+  end
+
+  def not_equal(word)
+    @root_word.casecmp(word) != 0
+  end
+
+  def has_same_letters(word)
+    word.downcase.split(//).sort == @root_word.downcase.split(//).sort
+  end
 end
