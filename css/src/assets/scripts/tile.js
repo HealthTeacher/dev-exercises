27065f9a9879aee4b17b8def91b03f8c2b324e3b
David Kraines <David.Kraines@blackboard.com>
2016-02-13 20:32:12 -0500

    Accessibility fixes for tabbing and enter key usage.

--- css/src/assets/scripts/tile.js
+++ css/src/assets/scripts/tile.js
@@ -1,8 +1,15 @@
-function toggleFavoritedClass(e) {
+function buttonClicked(e) {
   e.classList.toggle('favorited');
   e.classList.add('button-clicked');
 };
-// allow focus outline on favorite button when tabbed to
-function removeButtonClicked(e) {
+//accessibility fix
+function buttonKeyed(e) {
+  // make sure the key detection is cross browser compatible
+  var keyPressed = event.charCode ? event.charCode : event.keyCode ? event.keyCode : event.which ? event.which : 0;
+  // toggle favorited class if enter is pressed
+  if (keyPressed == 13 || keyPressed == 32) {
+    e.classList.toggle('favorited');
+  }
+  // allow focus outline on favorite button when tabbed to
   e.classList.remove('button-clicked');
 };
