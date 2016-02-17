eadd125b2ff2fd436c0e8113d4ac5a48e34962fb
David Kraines <David.Kraines@blackboard.com>
2016-02-17 10:54:50 -0500

    Cross browser fixes.
    className needed to replace classList for ie9 and some tabbing/press
    issues needed to be fixed. Also fixed link styling for ie9.

--- css/src/assets/scripts/tile.js
+++ css/src/assets/scripts/tile.js
@@ -1,21 +1,35 @@
-function buttonClicked(e) {
-  e.classList.toggle('favorited');
-  // remove outline when the button is clicked
-  e.classList.add('button-clicked');
+function buttonClicked(o) {
+  // classList doesn't work in ie 9 :(
+  // toggle favorited class and aria-pressed attribute
+  if(o.className == "favorite-button" || o.className == "favorite-button button-clicked" ){
+    o.className = "favorite-button favorited button-clicked";
+    o.setAttribute("aria-pressed", true);
+  }else {
+    o.className = "favorite-button button-clicked";
+    o.setAttribute("aria-pressed",  false);
+  }
 };
 
-function buttonKeyDown(e) {
+function buttonKeyDown(o) {
   // allow focus outline on favorite button when tabbed to
-  e.classList.remove('button-clicked');
-}
-
-function buttonKeyPressed(e) {
-
-  // make sure the key detection is cross browser compatible
-  var keyPressed = event.charCode ? event.charCode : event.keyCode ? event.keyCode : event.which ? event.which : 0;
-  // toggle favorited class if enter is pressed
-  if (keyPressed == 13 || keyPressed == 32) {
-    e.classList.toggle('favorited');
+  if(o.className == "favorite-button button-clicked"){
+    o.className = "favorite-button";
+  } else if(o.className == "favorite-button favorited button-clicked") {
+    o.className = "favorite-button favorited";
   }
+};
 
+function buttonKeyUp(o, e) {
+  // make sure the key detection is cross browser compatible
+  var keyPressed = e.charCode ? e.charCode : e.keyCode ? e.keyCode : e.which ? e.which : 0;
+  // toggle favorited class if enter or space is pressed
+  if (keyPressed == 13 || keyPressed == 32) {
+    if(o.className == "favorite-button"){
+      o.className = "favorite-button favorited";
+      o.setAttribute("aria-pressed", true);
+    } else if(o.className == "favorite-button favorited") {
+      o.className = "favorite-button";
+      o.setAttribute("aria-pressed",  false);
+    }
+  }
 };
