5253e5f73e2cde4b554ad94f779b765b01f39c5c
David Kraines <David.Kraines@blackboard.com>
2016-02-17 08:26:02 -0500

    Final accessibility tweaks.

--- css/src/assets/scripts/tile.js
+++ css/src/assets/scripts/tile.js
@@ -1,15 +1,21 @@
 function buttonClicked(e) {
   e.classList.toggle('favorited');
+  // remove outline when the button is clicked
   e.classList.add('button-clicked');
 };
-//accessibility fix
-function buttonKeyed(e) {
+
+function buttonKeyDown(e) {
+  // allow focus outline on favorite button when tabbed to
+  e.classList.remove('button-clicked');
+}
+
+function buttonKeyPressed(e) {
+
   // make sure the key detection is cross browser compatible
   var keyPressed = event.charCode ? event.charCode : event.keyCode ? event.keyCode : event.which ? event.which : 0;
   // toggle favorited class if enter is pressed
   if (keyPressed == 13 || keyPressed == 32) {
     e.classList.toggle('favorited');
   }
-  // allow focus outline on favorite button when tabbed to
-  e.classList.remove('button-clicked');
+
 };
