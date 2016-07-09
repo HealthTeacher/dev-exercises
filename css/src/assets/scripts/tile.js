c901ef635a5b9dac01887bb2ad1154bf9a09f426
Theo Pizza <theo.pizza@gmail.com>
2016-07-08 23:28:50 -0400

    refactored animations by migrating from GSAP to CSS3 only

--- css/src/assets/scripts/tile.js
+++ css/src/assets/scripts/tile.js
@@ -13,28 +13,18 @@
 			e.target.favorited = e.target.favorited ? false : true;
 			if(e.target.favorited){
 				$.addClass(e.target.parentNode.parentNode, 'tile--favorited');
-				TweenMax.fromTo(e.target,0.2,	{scale:0.88},
-										{scale:1});
-
-				TweenMax.fromTo(e.target, 0.3, {
-				    boxShadow: "0px 0px 0px 0px rgba(235,34,38,1)"
-				}, {
-				    boxShadow: "0px 0px 5px 10px rgba(235,34,38,0)",
-				    repeat: 0,
-				    ease: Linear.easeNone
-				});
-			}
-			else
+			} else {
 				$.removeClass(e.target.parentNode.parentNode, 'tile--favorited');
+			}
 		}
 
 		function contentHover(e){
-			TweenMax.to($.select('.tile-button--favorite', e.target.parentNode), 0.4, {autoAlpha: 1});
+			$.addClass(e.target.parentNode, 'tile--hovered');
 		}
 
 		function contentExit(e){
 			if(!$.hasClass(e.target.parentNode, 'tile--favorited'))
-				TweenMax.to($.select('.tile-button--favorite', e.target.parentNode), 0.4, {autoAlpha: 0});
+				$.removeClass(e.target.parentNode, 'tile--hovered');
 		}
 	};
 })(window.util);
